3dc94126f8f455e48a5235bae3c71ca96827c994
present2apple <present2apple@gmail.com>
2019-10-25 15:07:46 +0300

    modified:   Tropic-Island/C++/Project1/Project1/Project1.vcxproj 	modified:   Tropic-Island/C++/Project1/Project1/main.cpp 	new file:   Tropic-Island/C++/Project1/Project1/main2.cpp 	modified:   Tropic-Island/C++/Project1/Project1/playlist.m3u8 	modified:   Tropic-Island/C++/Project1/Project1/playlist2.m3u8 	new file:   Tropic-Island/C++/Project1/Project1/playlist4.m3u8 	new file:   Tropic-Island/C++/Project1/Project1/playlist5.m3u8 	modified:   Tropic-Island/Java/apache_tomcat/apache-tomcat-9.0.27/logs/localhost_access_log.2019-10-22.txt

--- Tropic-Island/C++/Project1/Project1/main2.cpp
+++ Tropic-Island/C++/Project1/Project1/main2.cpp
@@ -1,11 +1,9 @@
-
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <Windows.h>
 #include <string>
 #include <cstdlib>
-#include <algorithm>
 //#include <sstream>
 //#include <locale>
 #include <time.h>
@@ -20,12 +18,9 @@ struct Type
 };
 void GetFileList(LPTSTR sPath, LPTSTR sExt, LPTSTR sEXT, std::vector<Type> *v);
 
-void AddTalk(std::vector<Type> *src, std::vector<Type> *dst, float &currentlength, float maxlength);
-void AddMusic(std::vector<Type> *src, std::vector<Type> *dst, float &currentlength, float maxlength);
-int Random(std::vector<Type> *vector);
 int main(int argc, char* argv[])
 {
-	/*
+	
 	std::vector<Type> *vector = new std::vector<Type>();
 	std::vector<Type> *vectortalk = new std::vector<Type>();
 	char sPath[MAX_PATH] = "Z:\\Автоматизация\\музыка для радио от Сергея Косякова";
@@ -54,7 +49,7 @@ int main(int argc, char* argv[])
 	fout.write((*vector)[0].filename.c_str(), (*vector)[0].filename.size());
 	fout.write("\n", strlen("\n"));
 
-	
+	/*
 	std::string f_str;
 	for (int i = 0;i < vector->size();i++)
 	{
@@ -76,12 +71,12 @@ int main(int argc, char* argv[])
 		fout.write(f_str.c_str(), f_str.size());
 		fout.write("\n", 1);
 	}
-	
+	*/
 
 	fout.close();
-	*/
+
 	Type elem;
-	/*
+	
 	MP3 mp3__("C:\\Users\\User\\Desktop\\РАДИО\\джингл1.mp3");
 	std::cout << mp3__.get_name();
 	std::cout << mp3__.get_artist();
@@ -91,14 +86,13 @@ int main(int argc, char* argv[])
 	//mp3.save_pic("where/you/want/to/save.jpg");
 	elem.filename = "C:\\Users\\User\\Desktop\\РАДИО\\джингл1.mp3";
 	elem.length = mp3__.get_len();
-	*/
 	std::vector<Type> *vector2 = new std::vector<Type>();
 
-	//vector2->push_back(elem);
+	vector2->push_back(elem);
 	
 	setlocale(LC_ALL, "Russian");
 
-	std::ifstream fin("playlist4.m3u8");
+	std::ifstream fin("playlist2.m3u8");
 	if (!fin.is_open())
 	{
 		std::cout << "File not found!" << std::endl;
@@ -126,7 +120,7 @@ int main(int argc, char* argv[])
 
 	//setlocale(LC_ALL, "ru_RU.UTF-8");
 	std::vector<Type> *vector3 = new std::vector<Type>();
-	std::ifstream fin2("playlist5.m3u8");
+	std::ifstream fin2("playlist3.m3u8");
 	if (!fin2.is_open())
 	{
 		std::cout << "File not found!" << std::endl;
@@ -149,110 +143,17 @@ int main(int argc, char* argv[])
 	
 	std::cout << (*vector3)[0].filename << std::endl << (*vector3)[0].length << std::endl;
 	
-	std::sort(vector2->begin(), vector2->end(), [](const Type& a, const Type& b) {return a.length > b.length; });
-
-	for (int i = 0; i < vector2->size(); i++)
-		std::cout << vector2->at(i).length << " ";
-	float length = 3600.0, currentlength = 0.0f;
-	std::vector<Type> *vec = new std::vector<Type>();
-
-	srand(time(0));
-	AddMusic(vector2, vec, currentlength, length);
-	std::cout <<"vec: "<< std::endl;
-	for (int i = 0; i < vec->size(); i++)
-		std::cout << vec->at(i).length << " ";
-	std::cout << std::endl << "cl:" << currentlength << std::endl;
-	
-	std::sort(vector3->begin(), vector3->end(), [](const Type& a, const Type& b) {return a.length > b.length; });
-	for (int i = 0; i < vector3->size(); i++)
-		std::cout << vector3->at(i).length << " ";
 
 	
-	length = 2.5 * 3600.0;
-	AddTalk(vector3, vec, currentlength, length);
-	AddMusic(vector2, vec, currentlength, length);
-	std::cout << "vec: " << std::endl;
-	for (int i = 0; i < vec->size(); i++)
-		std::cout << vec->at(i).length << " ";
-	std::cout << std::endl << "cl:" << currentlength << std::endl;
-
-	length = 10800.0;
-	//
-	AddTalk(vector3, vec, currentlength, length);
-	//
-	AddMusic(vector2, vec, currentlength, length);
-	std::cout << "vec: " << std::endl;
-	for (int i = 0; i < vec->size(); i++)
-		std::cout << vec->at(i).length << " ";
-	std::cout << std::endl << "cl:" << currentlength << std::endl;
-	
-	std::ofstream fout;
-	fout.open("playlist.m3u8");
-	if (!fout.is_open())
-	{
-		std::cout << "Can't open file" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-	fout.write("#EXTM3U\n", strlen("#EXTM3U\n"));
-	for (int i = 0; i < vec->size(); i++)
-	{
-		fout.write("#EXTINF:", strlen("#EXTINF:"));
-		str = std::to_string((int)(*vec)[i].length);
-		fout.write(str.c_str(), strlen(str.c_str()));
-		fout.write(",", strlen(","));
-		fout.write((*vec)[i].filename.c_str(), (*vec)[i].filename.size());
-		fout.write("\n", strlen("\n"));
-		fout.write((*vec)[i].filename.c_str(), (*vec)[i].filename.size());
-		fout.write("\n", strlen("\n"));
-	}
-	fout.close();
-	//vector->clear();
-	//vectortalk->clear();
-
+	vector->clear();
+	vectortalk->clear();
 	std::cin.get();
 	return 0;
-}
-void AddTalk(std::vector<Type> *src,std::vector<Type> *dst, float &currentlength,float maxlength)
-{
-	int num;
-	bool b = false;
-	for (int i = 0; i < 1; i++)
-	{
-		num = Random(src);
-		if (src->at(num).length + currentlength <= maxlength)
-		{
-			b = true;
-			dst->push_back(src->at(num));
-			currentlength += dst->back().length;
-		}
-		if (!b)
-			AddTalk(src, dst, currentlength, maxlength);
-	}
-}
-void AddMusic(std::vector<Type> *src, std::vector<Type> *dst, float &currentlength, float maxlength)
-{
-	int num;
-	for (int i = 0; i < 10; i++)
-	{
-		num = Random(src);
-		if (src->at(num).length + currentlength <= maxlength)
-		{
-			dst->push_back(src->at(num));
-			currentlength += dst->back().length;
-		}
-	}
-	for (int i = 0; i < src->size(); i++)
-	{
-		if (src->at(i).length + currentlength <= maxlength)
-		{
-			dst->push_back(src->at(i));
-			currentlength += dst->back().length;
-		}
-	}
 }
 int Random(std::vector<Type> *vector)
 {
 	int min = 0, max = vector->size()-1;
+	srand(time(0));
 	return min + rand() % (max - min + 1);
 }
 void GetFileList(LPTSTR sPath, LPTSTR sExt, LPTSTR sEXT,std::vector<Type> *v) {
